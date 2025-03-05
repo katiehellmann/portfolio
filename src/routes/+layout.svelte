8f0eee712a8fd1d06b9de7acd66d61eb48aab447
katiehellmann <kmh9516@g.rit.edu>
2025-03-04 23:29:41 -0500

    ...

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,10 @@
-<script>
+<script lang="ts">
 	import '../app.css';
 	import Footer from "$lib/components/footer.svelte";
 	import Nav from "$lib/components/nav.svelte";
 	import { page } from '$app/stores';
 	let { children } = $props();
-	// This can be false if you're using a fallback (i.e. SPA mode)
-export const prerender = true;
-
 </script>
-
 
 {#if $page.url.pathname == '/'}
 	<Nav href=""/>
